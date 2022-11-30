6df7bae4aabad38143ae170a08d1399dba45dd78
Kirill Ki <kirillkalapov@gmail.com>
2022-11-30 18:09:46 +0100

    Initial Commit

---
+++ Mooi/MooiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MooiApp.swift
+//  Mooi
+//
+//  Created by  Mr.Ki on 30.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MooiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
